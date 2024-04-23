ba7f142cee23b67f09a3ba2cb854e9d05aeb3f2e
ssminami28 <ssminami28@gmail.com>
2024-04-23 19:21:21 +0900

    テストコード記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,8 @@
 class ItemsController < ApplicationController
+  def destroy
+    return unless user_signed_in? && current_user.id == @item.user_id
+
+    @item.destroy
+    redirect_to root_path
+  end
 end
