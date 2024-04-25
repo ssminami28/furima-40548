fff42b02cb15a401f8d086aac5160a5625ed4ebf
ssminami28 <ssminami28@gmail.com>
2024-04-25 21:50:22 +0900

    テストコード記述

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    title { Faker::Name.initials }
+    description { Faker::Lorem.sentence }
+    category_id { '2' }
+    sales_status_id { '2' }
+    shipping_fee_status_id { '2' }
+    prefecture_id { '2' }
+    scheduled_delivery_id { '2' }
+    price { '2222' }
+    association :user
+
+    after(:build) do |message|
+      message.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -42,7 +42,7 @@ RSpec.describe User, type: :model do
       it '・メールアドレスに@を含まない場合は登録できない' do
         @user.email = 'abc12.com'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Email is invalid")
+        expect(@user.errors.full_messages).to include('Email is invalid')
       end
       it 'メールアドレスが空欄だと保存できない' do
         @user.email = ''
