7815558a115df28265bd0a00b3256fb191367609
ssminami28 <ssminami28@gmail.com>
2024-04-26 19:02:05 +0900

    詳細ページ遷移

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  resources :items, only: [:index, :new, :create, :post]
+  resources :items, only: [:index, :new, :create, :post, :show]
 end
