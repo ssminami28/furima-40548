7b75cd8c64892325c8e6a7f7a1d451f7f30f6e70
ssminami28 <ssminami28@gmail.com>
2024-04-26 23:52:50 +0900

    hoge修正、変更するボタンを押したら詳細へ移動

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -20,6 +20,15 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item.update(item_params)
+    redirect_to item_path(@item)
   end
 
   private
