2e6c4c79158b19f1f2c1ce1ca618b65130990c2f
ssminami28 <ssminami28@gmail.com>
2024-04-27 17:25:09 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,7 +37,6 @@ class ItemsController < ApplicationController
   end
   
   def destroy
-    @item = Item.find(params[:id])
     if @item.user == current_user
       @item.destroy
       redirect_to root_path
