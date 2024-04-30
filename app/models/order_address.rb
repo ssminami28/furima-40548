6679f5eaefedf30fcfd17dd1a93af40399dc2208
ssminami28 <ssminami28@gmail.com>
2024-04-30 11:26:46 +0900

    修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -7,7 +7,7 @@ class OrderAddress
     validates :token, :city_name, :block_name, :user_id, :item_id
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid' }
-    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
+    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid' }
   end
 
   def save
