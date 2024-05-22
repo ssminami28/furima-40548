610496a94c3036b85bbae92cc2bc809c87aaa7e1
ssminami2288 <165633995+ssminami28@users.noreply.github.com>
2024-05-22 21:18:51 +0900

    amazon c3変更

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,7 +5,7 @@ class OrdersController < ApplicationController
 
   def index
     gon.public_key = ENV['PAYJP_PUBLIC_KEY']
-    @order_address = OrderAddress.new 
+    @order_address = OrderAddress.new
   end
 
   def create
